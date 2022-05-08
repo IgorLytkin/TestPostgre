ea9a878251e7a71384f083737d3a52197de54a0f
Igor Lytkin <igor.lytkin.2020@gmail.com>
2022-05-08 07:54:23 +0700

    Добавил операторы из курса DBA3

--- Replication.sql
+++ Replication.sql
@@ -10,3 +10,22 @@ FROM pg_stat_replication;
 select name, setting, setting::int * 16 || 'MB' AS setting_in_mb
 from pg_settings
 where name in ('min_wal_size', 'max_wal_size');
+
+
+SHOW server_version;
+
+\c - postgres
+SELECT type, database, user_name, auth_method FROM pg_hba_file_rules();
+
+CREATE DATABASE db1;
+CREATE TABLE t(
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    s text
+);
+INSERT INTO t(s) VALUES ('Привет, мир!'), (''), (NULL);
+COPY t TO stdout;
+COPY t TO stdout WITH (NULL '<NULL>', DELIMITER ',');
+COPY (SELECT * FROM t WHERE s IS NOT NULL) TO stdout;
+COPY t TO stdout WITH (FORMAT CSV);
+TRUNCATE TABLE t;
+ \pset null '\\N'

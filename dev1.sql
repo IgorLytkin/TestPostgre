11899d18cc14e312dcaa7e0a88c97459658b70ae
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-01-07 14:10:22 +0700

    Добавил объекты bookstore

--- dev1.sql
+++ dev1.sql
@@ -221,6 +221,19 @@ SELECT shorten(
     30
 );
 
+CREATE OR REPLACE FUNCTION author_name(
+    last_name text,
+    first_name text,
+    middle_name text
+) RETURNS text
+AS $$
+SELECT last_name || ' ' ||
+       left(first_name, 1) || '.' ||
+       CASE WHEN middle_name != '' -- подразумевает NOT NULL
+           THEN ' ' || left(middle_name, 1) || '.'
+           ELSE ''
+       END;
+$$ IMMUTABLE LANGUAGE sql;
 CREATE OR REPLACE FUNCTION book_name(book_id integer, title text)
 RETURNS text
 AS $$
@@ -1268,4 +1281,103 @@ CREATE TABLE tags(
     tag_id integer PRIMARY KEY,
     name text
 );
+
+-- Практика
+CREATE OR REPLACE FUNCTION add_book(title text, authors integer[])
+RETURNS integer
+AS $$
+DECLARE
+    book_id integer;
+    id integer;
+    seq_num integer := 1;
+BEGIN
+    INSERT INTO books(title)
+        VALUES(title)
+        RETURNING books.book_id INTO book_id;
+    FOREACH id IN ARRAY authors LOOP
+        INSERT INTO authorship(book_id, author_id, seq_num)
+            VALUES (book_id, id, seq_num);
+        seq_num := seq_num + 1;
+    END LOOP;
+    RETURN book_id;
+END;
+$$ VOLATILE LANGUAGE plpgsql;
+
+CREATE SCHEMA bookstore;
+ALTER DATABASE bookstore SET search_path = bookstore, public;
+\c bookstore
+SHOW search_path;
+CREATE TABLE authors(
+    author_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    last_name text NOT NULL,
+    first_name text NOT NULL,
+    middle_name text
+);
+CREATE TABLE books(
+    book_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title text NOT NULL
+);
+CREATE TABLE authorship(
+    book_id integer REFERENCES books,
+    author_id integer REFERENCES authors,
+    seq_num integer NOT NULL,
+    PRIMARY KEY (book_id,author_id)
+);
+CREATE TABLE operations(
+    operation_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    book_id integer NOT NULL REFERENCES books,
+    qty_change integer NOT NULL,
+    date_created date NOT NULL DEFAULT current_date
+);
+INSERT INTO authors(last_name, first_name, middle_name)
+VALUES
+    ('Пушкин', 'Александр', 'Сергеевич'),
+    ('Тургенев', 'Иван', 'Сергеевич'),
+    ('Стругацкий', 'Борис', 'Натанович'),
+    ('Стругацкий', 'Аркадий', 'Натанович'),
+    ('Толстой', 'Лев', 'Николаевич'),
+    ('Свифт', 'Джонатан', NULL);
+INSERT INTO books(title)
+VALUES
+    ('Сказка о царе Салтане'),
+    ('Муму'),
+    ('Трудно быть богом'),
+    ('Война и мир'),
+    ('Путешествия в некоторые удаленные страны мира в четырех частях: сочинение Лемюэля Гулливера, сначала хирурга, а затем капитана нескольких кораблей'),
+    ('Хрестоматия');
+INSERT INTO authorship(book_id, author_id, seq_num)
+VALUES
+    (1, 1, 1),
+    (2, 2, 1),
+    (3, 3, 2),
+    (3, 4, 1),
+    (4, 5, 1),
+    (5, 6, 1),
+    (6, 1, 1),
+    (6, 5, 2),
+    (6, 2, 3);
+COPY operations (operation_id, book_id, qty_change) FROM stdin;
+
+SELECT pg_catalog.setval('operations_operation_id_seq', 3, true);
+
+CREATE VIEW authors_v AS
+SELECT a.author_id,
+       a.last_name || ' ' ||
+       a.first_name ||
+       coalesce(' ' || nullif(a.middle_name, ''), '') AS display_name
+FROM   authors a;
+CREATE VIEW catalog_v AS
+SELECT b.book_id,
+       b.title AS display_name
+FROM   books b;
+CREATE VIEW operations_v AS
+SELECT book_id,
+       CASE
+           WHEN qty_change > 0 THEN 'Поступление'
+           ELSE 'Покупка'
+       END op_type,
+       abs(qty_change) qty_change,
+       to_char(date_created, 'DD.MM.YYYY') date_created
+FROM   operations
+ORDER BY operation_id;
 
